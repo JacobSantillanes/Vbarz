51b8f79b1ff07c1b0496f70b88388f1fd0db9ddc
Jacob Santillanes <jacobdweird@gmail.com>
2026-06-06 19:41:27 -0700

    Add checkout notice telling customers to screenshot order and DM @Saboresgotsit

--- index.js
+++ index.js
@@ -419,6 +419,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     if (checkoutBtn) {
         checkoutBtn.addEventListener('click', () => {
             if (cart.length > 0) {
+                alert('📸 Almost done!\n\nScreenshot your order summary and send it to @Saboresgotsit on Instagram to complete your purchase.\n\nWe\'ll redirect you to Instagram now.');
                 window.location.href = 'https://instagram.com/Saboresgotsit';
             } else {
                 alert('Your bag is empty!');
